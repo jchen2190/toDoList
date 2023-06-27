77c9f54b28ffccd0f679c854e1b1c48477ef529f
Jon Chen <jchen2190@gmail.com>
2023-06-26 22:05:20 -0400

    saving tasks

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import { v4 as uuidV4 } from 'uuid'
 
 type Task = {
-  id: string
-  title: string
-  completed: boolean
+  id: string,
+  title: string,
+  completed: boolean,
   createdAt: Date
 }
 
 const list = document.querySelector<HTMLUListElement>("#list");
 const form = document.getElementById("#new-task-form") as HTMLFormElement | null;
 const input = document.querySelector<HTMLInputElement>("#new-task-title");
+const tasks: Task[] = loadTasks()
+tasks.forEach(addListItem)
 
 form?.addEventListener("submit", e => {
   e.preventDefault();
@@ -20,18 +22,36 @@ form?.addEventListener("submit", e => {
     id: uuidV4(),
     title: input.value,
     completed: false,
-    createdAt: new Date()
+    createdAt: new Date(),
   }
+  tasks.push(newTask)
+
 
   addListItem(newTask)
+  input.value = ""
 })
 
 function addListItem(task: Task) {
   const item = document.createElement("li")
-  const label = document.createElement("label");
-  const checkbox = document.createElement("input");
-  checkbox.type = "checkbox";
+  const label = document.createElement("label")
+  const checkbox = document.createElement("input")
+  checkbox.addEventListener("change", () => {
+    task.completed = checkbox.checked
+    saveTasks()
+  })
+  checkbox.type = "checkbox"
+  checkbox.checked = task.completed;
   label.append(checkbox, task.title)
   item.append(label)
   list?.append(item)
+}
+
+function saveTasks() {
+  localStorage.setItem("TASKS", JSON.stringify(tasks));
+}
+
+function loadTasks(): Task[] {
+  const taskJSON = localStorage.getItem("TASKS")
+  if (taskJSON == null) return []
+  return JSON.parse(taskJSON)
 }
